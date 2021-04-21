8229cc564f3712bf6b99ef68356c841e2a53d249
chris-langfield <christopher.langfield@gmail.com>
2021-04-20 20:34:40 -0400

    Initial Commit

---
+++ BlueVM/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  BlueVM
+//
+//  Created by Christopher Langfield on 4/20/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
