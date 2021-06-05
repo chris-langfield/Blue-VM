eebe25573f294170e5307ac69a7c4695e5f76fec
Chris Langfield <34426450+chris-langfield@users.noreply.github.com>
2021-06-05 17:26:42 -0400

    Update main.c

--- BlueVM/main.c
+++ BlueVM/main.c
@@ -5,8 +5,6 @@
 //  Created by Christopher Langfield on 4/14/21.
 //
 
-// test gitignore
-//https://blog.subnetzero.io/post/building-language-vm-part-01/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
